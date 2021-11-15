181c03003477ade8d6b20d38133f0d70322376e8
HeleneElena <heleneebert1@gmail.com>
2021-11-15 19:50:24 +0100

    commit4

--- script/script.js
+++ script/script.js
@@ -7,19 +7,21 @@ document.addEventListener('DOMContentLoaded', function() {
           questionTitle = document.querySelector('#question'),
           formAnswers = document.querySelector('#formAnswers');
 
-    const questions = {
-        question: 'Какого цвета бургер вы хотите?',
-        answers: [
-            {
-                title: 'Стандарт',
-                url: './image/burger.png'
-            },
-            {
-                title: 'Черный',
-                url: './image/burgerBlack.png'
-            }
-        ]
-    };
+    const questions = [
+        {
+            question: 'Какого цвета бургер вы хотите?',
+            answers: [
+                {
+                    title: 'Стандарт',
+                    url: './image/burger.png'
+                },
+                {
+                    title: 'Черный',
+                    url: './image/burgerBlack.png'
+                }
+            ]
+        }
+    ];
           
     btnOpenModal.addEventListener('click', () => {
         modalBlock.classList.add('d-block');
@@ -29,25 +31,12 @@ document.addEventListener('DOMContentLoaded', function() {
     closeModal.addEventListener('click', () => {
         modalBlock.classList.remove('d-block');
     });
-
+    
+    // функция запуска тестирования
     const playTest = () => {
-        const renderQuestions = () => {
-            questionTitle.textContent = `${questions.question}`;
 
-            formAnswers.innerHTML = `
-                <div class="answers-item d-flex flex-column">
-                    <input type="radio" id="answerItem1" name="answer" class="d-none">
-                    <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                        <img class="answerImg" src="${questions.answers[0].url}" alt="burger">
-                        <span>${questions.answers[0].title}</span>
-                    </label>
-                </div>
-            `;
-        };
-        renderQuestions();
-
-    const renderAnswers = () => {
-        for (i = 0; i < 2; i++) {
+        const renderAnswers = () => {
+            questions[0].answers.forEach((answer) => {
             const answerItem = document.createElement('div');
 
             answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
@@ -55,17 +44,22 @@ document.addEventListener('DOMContentLoaded', function() {
             answerItem.innerHTML = `
                     <input type="radio" id="answerItem1" name="answer" class="d-none">
                     <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                        <img class="answerImg" src="${questions.answers[0].url}" alt="burger">
-                        <span>${questions.answers[0].title}</span>
+                        <img class="answerImg" src="${answer.url}" alt="burger">
+                        <span>${answer.title}</span>
                     </label>            
             `;
             formAnswers.appendChild(answerItem);
+          });
+        };
+
+        const renderQuestions = () => {
+            questionTitle.textContent = `${questions[0].question}`;
+
+            renderAnswers();
+        };
+        renderQuestions();
 
 
-            console.log(answerItem);
-        }
-    }
-    renderQuestions();
     };
 
 });
